1bc167f418d8128141eb7758884216702ca9e40e
VL <3278714897@qq.com>
2021-09-06 01:00:48 +0000

    update GPCR-KD-PRO/GPCR-KD-master/seqToResult/main.py.

--- GPCR-KD-PRO/GPCR-KD-master/seqToResult/main.py
+++ GPCR-KD-PRO/GPCR-KD-master/seqToResult/main.py
@@ -99,7 +99,7 @@ except Exception as e:
 
 print(time.ctime(), ":", "开始训练!")
 
-classifier_factory = ["MLPClassifier(hidden_layer_sizes=(2000,2000), max_iter=1000)","RandomForestClassifier","SVC","GaussianNB"]
+classifier_factory = ["MLPClassifier(hidden_layer_sizes=(2000,2000), max_iter=1000)","RandomForestClassifier()","SVC()","GaussianNB()"]
 
 clf = SVC()
 clf.fit(x_train, y_train)
